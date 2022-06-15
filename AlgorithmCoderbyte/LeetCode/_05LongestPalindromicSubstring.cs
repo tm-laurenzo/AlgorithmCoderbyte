cc44f13c902002dfb5567d36170cd055b7aa47b5
Chibuike Chidolue <t.m.laurenzo@gmail.com>
2022-06-15 23:15:00 +0100

    Longestsubstring palindrome question

---
+++ AlgorithmCoderbyte/LeetCode/_05LongestPalindromicSubstring.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCoderbyte.LeetCode
+{
+    public static class _05LongestPalindromicSubstring
+    {
+        public static string LongestPalindrome(string s)
+        {
+            var listOfSubstrings = new List<string>();
+            for(int firstPointer = 0; firstPointer < s.Length; firstPointer++)
+            {
+
+
+
+                for(int secondPointer = firstPointer + 1; secondPointer < s.Length; secondPointer++)
+                {
+
+                }
+            }
+
+            return "goat";
+        }
+    }
+}
