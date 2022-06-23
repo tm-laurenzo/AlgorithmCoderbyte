45f713b03f3f27467ebb66700c47f9aa91c74028
Chibuike Chidolue <t.m.laurenzo@gmail.com>
2022-06-23 23:49:22 +0100

    Marking register for HR guys

--- AlgorithmCoderbyte/LeetCode/_05LongestPalindromicSubstring.cs
+++ AlgorithmCoderbyte/LeetCode/_05LongestPalindromicSubstring.cs
@@ -53,6 +53,7 @@ namespace AlgorithmCoderbyte.LeetCode
 
         private static int expandAroundCenter(String s, int left, int right)
         {
+
             int L = left, R = right;
             while (L >= 0 && R < s.Length && s.[L] == s.[R])
             {
