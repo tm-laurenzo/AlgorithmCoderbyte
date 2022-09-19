77aed5d237fb1b588744543055773450aa9ceda7
Chibuike Chidolue <t.m.laurenzo@gmail.com>
2022-09-19 18:08:12 +0100

    zigzag in javascript implementation

---
+++ AlgorithmCoderbyte/LeetCode JavaScript/06 ZigZag Conversion.js
@@ -0,0 +1,28 @@
+﻿var convert = function(s, numRows) {
+    //create an empty array
+    let arr = [];
+    //make the array to be of type string and make the leght to be or numRow
+    for(let i = 0; i < numRows; i++){
+        arr.push("");
+    }
+    let i = 0;
+    while(i < s.length){
+        for(let j = 0; i < s.length && j < numRows; j++){
+            arr[j] += s[i++];
+        }
+        for(let j = numRows - 2; i < s.length && j < numRows; j++){
+            arr[j] += s[i++];
+        }
+
+    }
+
+    let answer = "";
+    arr.forEach(element => {
+        console.log(element);
+        answer += element;
+    });
+    
+    return answer;
+};
+console.log("Expected PAHNAPLSIIGYIR");
+console.log(convert("PAYPALISHIRING",3));
