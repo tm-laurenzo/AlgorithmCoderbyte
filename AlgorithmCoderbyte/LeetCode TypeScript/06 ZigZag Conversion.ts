2a27d05b5ecefeb014fd9508586ebd6fc784ad5f
Chibuike Chidolue <t.m.laurenzo@gmail.com>
2022-09-20 15:42:41 +0100

    06 zigzag ts done

--- AlgorithmCoderbyte/LeetCode TypeScript/06 ZigZag Conversion.ts
+++ AlgorithmCoderbyte/LeetCode TypeScript/06 ZigZag Conversion.ts
@@ -3,12 +3,16 @@ function convert(s: string, numRows: number): string {
   let arr: string[] = [];
   let i: number = 0;
 
+  for(let i = 0; i < numRows; i++){
+    arr.push("");
+ }
+
   while (i < s.length) {
     for (let j = 0; i < s.length && j < numRows; j++) {
-      arr[j] = s[i++];
+      arr[j] += s[i++];
     }
     for (let j = numRows - 2; i < s.length && j > 0; j--) {
-      arr[j] = s[i++];
+      arr[j] += s[i++];
     }
   }
   arr.forEach((elememt) => {
