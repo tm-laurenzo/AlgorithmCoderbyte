0b53f68621af5234387ef437e8570e9dd05f459c
tm-laurenzo <60081627+tm-laurenzo@users.noreply.github.com>
2023-01-23 22:50:45 +0100

    Added Leetcode 14 Longest Common Prefix in C#

--- AlgorithmCoderbyte/LeetCode C-sharp/0014 LeetCode 14. Longest Common Prefix.cs
+++ AlgorithmCoderbyte/LeetCode C-sharp/0014 LeetCode 14. Longest Common Prefix.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace AlgorithmCoderbyte.LeetCode_C_sharp
 {
-    class _0014_LeetCode_14
+    public static class _0014_LeetCode_14
     {
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0) return "";
+            string prefix = strs[0];
+            for(int  i = 1; i < strs.Length; i++)
+            {
+                while(strs[i].IndexOf(prefix) != 0)
+                {
+                    prefix = prefix.Substring(0, prefix.Length - 1);
+                }
+            }
+            return prefix;
+        }
+        public static void Run()
+        {
+            //Console.WriteLine(MyAtoi(" - 91283472332"));
+            //Console.WriteLine(MyAtoi("words and 987"));
+            //Console.WriteLine(MyAtoi("+-12"));
+            //Console.WriteLine(MyAtoi("-abc"));
+            Console.WriteLine(LongestCommonPrefix("1"));
+        }
     }
 }
