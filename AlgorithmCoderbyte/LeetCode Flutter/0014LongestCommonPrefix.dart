22bf45e60b8f2611c92ae5e270942e987d34f9ad
tm-laurenzo <60081627+tm-laurenzo@users.noreply.github.com>
2024-06-05 09:53:50 +0100

    LC 14 fixed some space in dart

--- AlgorithmCoderbyte/LeetCode Flutter/0014LongestCommonPrefix.dart
+++ AlgorithmCoderbyte/LeetCode Flutter/0014LongestCommonPrefix.dart
@@ -1,6 +1,7 @@
 String longestCommonPrefix(List<String> strs) {
   if (strs.isEmpty) return "";
   String prefix = strs[0];
+  
   for (int i = 1; i < strs.length; i++) {
     while (strs[i].indexOf(prefix) != 0) {
       prefix = prefix.substring(0, prefix.length - 1);
